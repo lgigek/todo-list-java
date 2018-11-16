6858e3f8d2b8146895b64c536515624cae0f1a3c
Lucas Gigek <lucasgigek@gmail.com>
2018-11-16 18:06:24 -0200

    added log to project

--- src/main/java/com/github/lgigek/controllers/TaskController.java
+++ src/main/java/com/github/lgigek/controllers/TaskController.java
@@ -2,6 +2,8 @@ package com.github.lgigek.controllers;
 
 import com.github.lgigek.models.Task;
 import com.github.lgigek.business.TaskBusiness;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,13 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TaskController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
+
     @Autowired
     private TaskBusiness taskBusiness;
 
     @RequestMapping(value = "create", method = RequestMethod.POST)
     public ResponseEntity createTask(@RequestBody Task task) {
 
+        logger.info("Creating a new task with name \"{}\"", task.getName());
+
         taskBusiness.createTask(task);
+
+        logger.info("Task created successfully");
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
