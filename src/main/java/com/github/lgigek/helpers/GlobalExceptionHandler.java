0277c344b919e174ab3938e693ebb71fde772d40
Lucas Gigek <lucasgigek@gmail.com>
2018-11-16 18:07:28 -0200

    created an exception handler for unknown errors

--- src/main/java/com/github/lgigek/helpers/GlobalExceptionHandler.java
+++ src/main/java/com/github/lgigek/helpers/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.github.lgigek.helpers;
 
 import com.github.lgigek.helpers.exceptions.TaskAlreadyCreatedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,8 +12,20 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity unknownError(Exception e) {
+
+        logger.error("An unknown exception has been thrown. Error: {}", e.getMessage());
+
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @ExceptionHandler(value = {TaskAlreadyCreatedException.class})
     public ResponseEntity taskAlreadyCreated(Exception e) {
+
+        logger.error("Failed to create the new task. Error: {}", e.getMessage());
 
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
